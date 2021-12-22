f7d95a01b4c6bf23e8896b46965b85bc539ecd8d
Miguel <gimenezgimenezmiguel@gmail.com>
2021-12-22 16:42:08 +0100

    add couriers module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CouriersModule } from './couriers/couriers.module';
 
 
 @Module({
-  imports: [],
+  imports: [CouriersModule],
   controllers: [],
   providers: [],
 })
