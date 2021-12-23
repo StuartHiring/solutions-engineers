3dfe92e619c1c96e1a7edfa51febf25516f8edf8
Miguel <gimenezgimenezmiguel@gmail.com>
2021-12-23 17:24:57 +0100

    add couriers module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 import ormconfig from './db/ormconfig'
+import { CouriersModule } from './couriers/couriers.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormconfig())
+    TypeOrmModule.forRoot(ormconfig()),
+    CouriersModule
   ],
   controllers: [],
   providers: [],
