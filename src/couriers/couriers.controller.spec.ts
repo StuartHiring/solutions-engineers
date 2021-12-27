3aa2a14038989fc375c57f76d8042854ab1c9cbe
Miguel <gimenezgimenezmiguel@gmail.com>
2021-12-27 09:58:22 +0100

    Add courier controller findCourier unit tests

--- src/couriers/couriers.controller.spec.ts
+++ src/couriers/couriers.controller.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CouriersController } from './couriers.controller';
+import { CouriersService } from './couriers.service';
+
 
 describe('CouriersController', () => {
   let controller: CouriersController;
+  let spyService: CouriersService;
+
+  const CourierServiceSpy = {
+    provide: CouriersService,
+    useFactory: () => ({
+      findCouriers: jest.fn(() => { }),
+    }),
+  }
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CouriersController],
+      providers: [CourierServiceSpy],
+
     }).compile();
 
     controller = module.get<CouriersController>(CouriersController);
+    spyService = module.get<CouriersService>(CouriersService);
+
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should call findCouriers', async () => {
+    controller.findCouriers();
+    expect(spyService.findCouriers).toHaveBeenCalled();
+  });
+
+  it('should call findCouriers with correct parameters', async () => {
+    const capacity_required = 45
+
+    controller.findCouriers({ capacity_required });
+    expect(spyService.findCouriers).toHaveBeenCalledWith({ capacity_required });
+  });
+
+
 });
